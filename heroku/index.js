aad8c62582bdac8294f80d45d2e48cd91260789c
Alan Ribeiro <alan.r.santos@outlook.com>
2023-02-06 02:17:32 -0300

    a

--- heroku/index.js
+++ heroku/index.js
@@ -40,7 +40,8 @@ app.get(['/facebook', '/instagram'], function(req, res) {
 
 app.post('/facebook', function(req, res) {
   console.log('Facebook request body:', req.body);
-
+  console.log(req)
+  console.log(req.getHeader("x-hub-signature"))
   if (!req.isXHubValid()) {
     console.log('Warning - request header X-Hub-Signature not present or invalid');
     res.sendStatus(401);
